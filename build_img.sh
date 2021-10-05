ea68f4ab8a81f984769aaf28139066dba2d18a51
SanyaNya <sanyanya4ever@gmail.com>
2021-10-05 17:26:57 +0300

    Echo kernel size

--- build_img.sh
+++ build_img.sh
@@ -1,3 +1,4 @@
 nasm -f elf32 bootloader.asm -o boot.o
 i386-elf-g++ -std=c++20 kmain.cpp boot.o -o kernel.bin -nostdlib -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -Wall -Wextra -Werror -T linker.ld
+echo "kernel size: $(wc -c < kernel.bin) bytes"
 ./create_kernel_bootsector
