e658199cc9f803bc3191505435d3bdb0530a7fdf
SanyaNya <sanyanya4ever@gmail.com>
2021-11-03 12:44:40 +0300

    Add cstdint

---
+++ libcpp/cstdint.hpp
@@ -0,0 +1,17 @@
+#ifndef STD_CSTDINT_HPP
+#define STD_CSTDINT_HPP
+
+namespace std
+{
+    using int8_t   = signed char;
+    using int16_t  = short;
+    using int32_t  = long;
+    using int64_t  = long long;
+
+    using uint8_t  = unsigned char;
+    using uint16_t = unsigned short;
+    using uint32_t = unsigned long;
+    using uint64_t = unsigned long long;
+}
+
+#endif //STD_CSTDINT_HPP
