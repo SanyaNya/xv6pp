d2f9be1920d817733c642cbd30ddf486cef83de9
SanyaNya <sanyanya4ever@gmail.com>
2021-12-01 17:52:27 +0300

    Determine flags in toolchain

--- toolchain-i386-elf-clang++.cmake
+++ toolchain-i386-elf-clang++.cmake
@@ -10,3 +10,21 @@ set(CMAKE_CXX_COMPILER_FORCED TRUE)
 
 add_compile_options($<$<COMPILE_LANGUAGE:CXX>:--target=i386-elf>)
 add_link_options($<$<COMPILE_LANGUAGE:CXX>:--target=i386-elf>)
+
+set(CXX_OPTIMIZATION_DEBUG_FLAGS      -O0;-g)
+set(CXX_OPTIMIZATION_STANDARD_FLAGS   -O2)
+set(CXX_OPTIMIZATION_AGGRESSIVE_FLAGS -O3)
+set(CXX_OPTIMIZATION_SIZE_FLAGS       -Os)
+
+set(CXX_NO_STDLIB_FLAG           -nostdlib)
+set(CXX_FREESTANDING_FLAG        -ffreestanding)
+set(CXX_NO_RED_ZONE_FLAG         -mno-red-zone)
+set(CXX_NO_EXCEPTIONS_FLAG       -fno-exceptions)
+set(CXX_NO_RTTI_FLAG             -fno-rtti)
+set(CXX_WALL_WEXTRA_WERROR_FLAGS -Wall;-Wextra;-Werror)
+
+function(target_cxx_linker_script TARGET LINKER_SCRIPT)
+    set_target_properties(${TARGET} PROPERTIES
+        LINK_FLAGS "-T ${LINKER_SCRIPT}"
+        LINK_DEPENDS ${LINKER_SCRIPT})
+endfunction()
--- toolchain-i386-elf-g++.cmake
+++ toolchain-i386-elf-g++.cmake
@@ -7,3 +7,21 @@ set(CMAKE_CXX_COMPILER i386-elf-g++)
 #Don`t check compiler
 set(CMAKE_C_COMPILER_FORCED TRUE)
 set(CMAKE_CXX_COMPILER_FORCED TRUE)
+
+set(CXX_OPTIMIZATION_DEBUG_FLAGS      -O0;-g)
+set(CXX_OPTIMIZATION_STANDARD_FLAGS   -O2)
+set(CXX_OPTIMIZATION_AGGRESSIVE_FLAGS -O3)
+set(CXX_OPTIMIZATION_SIZE_FLAGS       -Os)
+
+set(CXX_NO_STDLIB_FLAG           -nostdlib)
+set(CXX_FREESTANDING_FLAG        -ffreestanding)
+set(CXX_NO_RED_ZONE_FLAG         -mno-red-zone)
+set(CXX_NO_EXCEPTIONS_FLAG       -fno-exceptions)
+set(CXX_NO_RTTI_FLAG             -fno-rtti)
+set(CXX_WALL_WEXTRA_WERROR_FLAGS -Wall;-Wextra;-Werror)
+
+function(target_cxx_linker_script TARGET LINKER_SCRIPT)
+    set_target_properties(${TARGET} PROPERTIES
+        LINK_FLAGS "-T ${LINKER_SCRIPT}"
+        LINK_DEPENDS ${LINKER_SCRIPT})
+endfunction()
