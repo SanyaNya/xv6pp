1c5822fb434525dc24155df6efee4ab69207e2cf
SanyaNya <sanyanya4ever@gmail.com>
2021-12-14 17:09:27 +0300

    Section header

--- ELF/Section.hpp
+++ ELF/Section.hpp
@@ -1,10 +1,58 @@
 #ifndef ELF_SECTION_HPP
 #define ELF_SECTION_HPP
 
+#include "../libcpp/cstdint.hpp"
+
 namespace ELF::Section
 {
 
+enum class Type : std::uint32_t
+{
+    NULL,     //Inactive, other members undefined
+    PROGBITS, //Info about program
+    SYMTAB,   //Symbol Table
+    STRTAB,   //String Table
+    RELA,     //Relocation entries with explicit addends
+    HASH,     //Symbol hash table
+    DYNAMIC,  //Dynamic linking info
+    NOTE,     //Notes
+    NOBITS,   //Like PROGBITS but occupies no space in the file
+    REL,      //Relocation offsets without explicit addends
+    SHLIB,    //RESERVED
+    DYNSYM,   //Set of dynamic linking symbols
+    
+    LOPROC = 0x70000000, //RESERVED for processor specific
+    HIPROC = 0x7fffffff, //
 
+    LOUSER = 0x80000000, //RESERVED for application
+    HIUSER = 0xffffffff  //
+
+};
+
+enum class FlagsMask : std::uint32_t
+{
+    WRITE = 1,
+    ALLOC = 2,
+    EXECINSTR = 4,
+    PROC = 0xf0000000
+};
+
+struct Header
+{
+    std::uint32_t name_idx;
+    Type type;
+    std::uint32_t flags;
+    std::uint8_t* addr;   //If section don`t appears in the memory addr = 0
+    std::uint32_t offset; //Offset from the beginning of the file
+    std::uint32_t size;   //Size of this section.
+    std::uint32_t link;   //Section Header table index(interpretation depends on section type)
+    std::uint32_t info;   //Extra info(interpretation depends on section type)
+    std::uint32_t align;  //align should be positive and power of two
+                          //if align = 0 or align = 1, then no alignment required
+
+    std::uint32_t entsize; //Size of entry in table.
+                           //If entsize = 0, there is no table
+};
 
 } //namespace ELF::Section
 
