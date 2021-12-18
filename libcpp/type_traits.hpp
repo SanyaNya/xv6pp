161cefbc7b1dc45fbb52432417c1f042b43fb885
SanyaNya <sanyanya4ever@gmail.com>
2021-12-18 13:55:15 +0300

    Add some stuff

--- libcpp/type_traits.hpp
+++ libcpp/type_traits.hpp
@@ -71,6 +71,34 @@ struct type_identity
     using type = T;
 };
 
+template<typename T1, typename T2>
+struct is_same : false_type {};
+
+template<typename T>
+struct is_same<T, T> : true_type {};
+
+template<typename T1, typename T2>
+inline constexpr bool is_same_v = is_same<T1, T2>::value;
+
+template<typename T>
+struct is_void : is_same<T, void> {};
+
+template<typename T>
+inline constexpr bool is_void_v = is_void<T>::value;
+
+template<typename ... Bs>
+struct conjunction : bool_constant<(Bs::value && ...)> {};
+
+template<typename From, typename To>
+struct is_explicitly_covertible : conjunction<is_void<From>, is_void<To>> {};
+
+template<typename From, typename To>
+    requires requires(From f) { static_cast<To>(f); }
+struct is_explicitly_convertible : true_type {};
+
+template<typename From, typename To>
+inline constexpr bool is_explicitly_convertible_v = is_explicitly_convertible<From, To>::value;
+
 
 template<typename E> requires is_enum_v<E>
 struct underlying_type : type_identity<__underlying_type(E)> {};
