213fb25fd45c204df9e2716f17de852d6edc938b
SanyaNya <sanyanya4ever@gmail.com>
2022-01-23 21:09:46 +0300

    Placement new

---
+++ libcpp/new.hpp
@@ -0,0 +1,22 @@
+#ifndef STD_NEW_HPP
+#define STD_NEW_HPP
+
+#include "cstddef.hpp"
+
+
+
+//placement new
+[[nodiscard]] inline void* operator new(std::size_t, void* p) noexcept
+{
+    return p;
+}
+
+[[nodiscard]] inline void* operator new[](std::size_t, void* p) noexcept
+{
+    return p;
+}
+
+inline void operator delete(void*, void*) noexcept {}
+inline void operator delete[](void*, void*) noexcept {}
+
+#endif //STD_NEW_HPP
