946dc9acfc1be5d48fab42a9bfd6e918fdab00c1
SanyaNya <sanyanya4ever@gmail.com>
2022-01-23 21:04:33 +0300

    Compiler-independent impl

--- libcpp/cstddef.hpp
+++ libcpp/cstddef.hpp
@@ -1,11 +1,13 @@
 #ifndef STD_CSTDDEF_HPP
 #define STD_CSTDDEF_HPP
 
+#include "utility.hpp"
+
 namespace std
 {
 
-using size_t = unsigned int;
-using ptrdiff_t = int;
+using size_t = decltype(sizeof(int));
+using ptrdiff_t = decltype(declval<int*>()-declval<int*>());
 using nullptr_t = decltype(nullptr);
 using max_align_t = long double;
 
