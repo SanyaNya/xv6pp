9d4d83447b9f39bfc43f792ebff6be664774ad78
SanyaNya <sanyanya4ever@gmail.com>
2022-02-01 17:18:49 +0300

    static_assert sizes

--- libcpp/cstdint.hpp
+++ libcpp/cstdint.hpp
@@ -12,6 +12,16 @@ namespace std
     using uint16_t = unsigned short;
     using uint32_t = unsigned int;
     using uint64_t = unsigned long long;
+
+    static_assert(sizeof(int8_t)  == 1);
+    static_assert(sizeof(int16_t) == 2);
+    static_assert(sizeof(int32_t) == 4);
+    static_assert(sizeof(int64_t) == 8);
+
+    static_assert(sizeof(uint8_t)  == 1);
+    static_assert(sizeof(uint16_t) == 2);
+    static_assert(sizeof(uint32_t) == 4);
+    static_assert(sizeof(uint64_t) == 8);
 }
 
 #endif //STD_CSTDINT_HPP
