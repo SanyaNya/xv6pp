c2333d8859702544b48bdd824459c38ac8333c9d
SanyaNya <sanyanya4ever@gmail.com>
2022-02-10 19:11:21 +0300

    enable_if_t

--- libcpp/type_traits_impl/enable_if.hpp
+++ libcpp/type_traits_impl/enable_if.hpp
@@ -12,6 +12,9 @@ struct enable_if {};
 template<typename T>
 struct enable_if<true, T> : type_identity<T> {};
 
+template<bool B, typename T = void>
+using enable_if_t = typename enable_if<B, T>::type;
+
 } //namespace std
 
 #endif //STD_TYPE_TRAITS_IMPL_ENABLE_IF_HPP
