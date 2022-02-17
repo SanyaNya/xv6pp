ea4083965dde9d8b97a1bca84cf5e77b3a485d06
SanyaNya <sanyanya4ever@gmail.com>
2022-02-17 16:36:38 +0300

    Move kernel stuff to dir

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,9 +1,9 @@
-#include "print.hpp"
+#include "../utils/print.hpp"
 
 namespace xv6pp
 {
 
-extern "C" [[noreturn]] void _start()
+extern "C" [[noreturn]] void kmain()
 {
     unsigned int i = 0;
     while(true)
