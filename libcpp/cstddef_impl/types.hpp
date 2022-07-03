5c4982757f89e5d0323ab72b53e5580fdfb87689
SanyaNya <sanyanya4ever@gmail.com>
2022-07-03 14:01:59 +0300

    Proper impl for max_align_t

--- libcpp/cstddef_impl/types.hpp
+++ libcpp/cstddef_impl/types.hpp
@@ -2,6 +2,7 @@
 #define STD_CSTDDEF_IMPL_TYPES_HPP
 
 #include "../utility_impl/declval.hpp"
+#include "../../utils/type_list.hpp"
 
 namespace std
 {
@@ -9,7 +10,39 @@ namespace std
 using size_t      = decltype(sizeof(int));
 using ptrdiff_t   = decltype(declval<int*>()-declval<int*>());
 using nullptr_t   = decltype(nullptr);
-using max_align_t = long double;
+
+namespace detail
+{
+
+using std_types = 
+    meta::type_list<
+        nullptr_t,
+        float,
+        double,
+        long double,
+        bool,
+        char,
+        signed char,
+        unsigned char,
+        char8_t,
+        char16_t,
+        char32_t,
+        wchar_t,
+        short int,
+        int,
+        long int,
+        long long int,
+        unsigned short int,
+        unsigned int,
+        unsigned long int,
+        unsigned long long int>;
+
+} //namespace detail
+
+
+using max_align_t = 
+    typename detail::std_types::template max<
+        []<typename A, typename B>(){ return (alignof(A) > alignof(B)); }>;
 
 } //namespace std
 
