14111265fe6eead56fe6053a732ef8a15a48837c
SanyaNya <sanyanya4ever@gmail.com>
2022-08-14 18:21:43 +0300

    Extract bitfield.hpp

--- utils/bitfield.hpp
+++ utils/bitfield.hpp
@@ -1,7 +1,7 @@
 #ifndef XV6PP_UTILS_BITFIELD_HPP
 #define XV6PP_UTILS_BITFIELD_HPP
 
-#include "../../libcpp/bit.hpp"
+#include "../libcpp/bit.hpp"
 
 namespace xv6pp
 {
@@ -12,16 +12,19 @@ struct bitfield
     constexpr bitfield() noexcept = default;
     constexpr bitfield(TAlias t) noexcept
     {
+        static_assert(sizeof(TImpl) == sizeof(TAlias));
         *static_cast<TImpl*>(this) = std::bit_cast<TImpl>(t); 
     }
 
     constexpr TImpl& operator=(TAlias t) noexcept
     {
+        static_assert(sizeof(TImpl) == sizeof(TAlias));
         return *static_cast<TImpl*>(this) = std::bit_cast<TImpl>(t);
     }
 
     constexpr operator TAlias() const noexcept
     {
+        static_assert(sizeof(TImpl) == sizeof(TAlias));
         return std::bit_cast<TAlias>(*static_cast<const TImpl*>(this)); 
     }
 };
